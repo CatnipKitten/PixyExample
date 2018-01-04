f95a5d0caec2c1de8c84a848e13a4e49c81deafe
Zach Smith <kerbonauts@gmail.com>
2018-01-04 16:35:59 -0700

    Added sample code for accessing the Pixy's data.

---
+++ PixyExample/Pixy/PixyCamera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixy
+{
+    public class PixyCamera
+    {
+        public bool IsConnected
+        {
+            get
+            {
+                int status = pixy.pixy_cam_get_auto_exposure_compensation();
+                if (status < 0) return false;
+                else return true;
+            }
+        }
+
+        public int Initialize()
+        {
+            return pixy.pixy_init();
+        }
+
+        public void Close()
+        {
+            pixy.pixy_close();
+        }
+
+        public int GetBlocks(ushort maxCount, BlockArray blocks)
+        {
+            return pixy.pixy_get_blocks(maxCount, blocks);
+        }
+    }
+}
--- PixyExample/PixyExample.cs
+++ PixyExample/PixyExample.cs
@@ -2,77 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using WPILib;
+using Pixy;
 using WPILib.SmartDashboard;
 
 namespace PixyExample
 {
-    /// <summary>
-    /// The VM is configured to automatically run this class, and to call the
-    /// functions corresponding to each mode, as described in the IterativeRobot
-    /// documentation. 
-    /// </summary>
     public class PixyExample : IterativeRobot
     {
-        const string defaultAuto = "Default";
-        const string customAuto = "My Auto";
-        string autoSelected;
-        SendableChooser chooser;
-
-        /// <summary>
-        /// This function is run when the robot is first started up and should be
-        /// used for any initialization code.
-        /// </summary>
+        PixyCamera Pixy;
         public override void RobotInit()
         {
-            chooser = new SendableChooser();
-            chooser.AddDefault("Default Auto", defaultAuto);
-            chooser.AddObject("My Auto", customAuto);
-            SmartDashboard.PutData("Chooser", chooser);
+            Pixy = new PixyCamera();
+            Pixy.Initialize();
+            SmartDashboard.PutBoolean("Pixy Status", Pixy.IsConnected);
         }
 
-        // This autonomous (along with the sendable chooser above) shows how to select between
-        // different autonomous modes using the dashboard. The senable chooser code works with
-        // the Java SmartDashboard. If you prefer the LabVIEW Dashboard, remove all the chooser
-        // code an uncomment the GetString code to get the uto name from the text box below
-        // the gyro.
-        //You can add additional auto modes by adding additional comparisons to the switch
-        // structure below with additional strings. If using the SendableChooser
-        // be sure to add them to the chooser code above as well.
         public override void AutonomousInit()
         {
-            autoSelected = (string)chooser.GetSelected();
-            //autoSelected = SmartDashboard.GetString("Auto Selector", defaultAuto);
-            Console.WriteLine("Auto selected: " + autoSelected);
+            SmartDashboard.PutBoolean("Pixy Status", Pixy.IsConnected);
+            BlockArray blocks = new BlockArray(2);
+            SmartDashboard.PutNumber("Pixy Count", Pixy.GetBlocks(2, blocks));
+
         }
 
-        /// <summary>
-        /// This function is called periodically during autonomous
-        /// </summary>
         public override void AutonomousPeriodic()
         {
-            switch (autoSelected)
-            {
-                case customAuto:
-                    //Put custom auto code here
-                    break;
-                case defaultAuto:
-                default:
-                    //Put default auto code here
-                    break;
-            }
+            BlockArray blocks = new BlockArray(2);
+            SmartDashboard.PutNumber("Pixy Count", Pixy.GetBlocks(2, blocks));
+            SmartDashboard.PutNumber("Pixy Area", blocks.getitem(0).height * blocks.getitem(0).width);
         }
 
-        /// <summary>
-        /// This function is called periodically during operator control
-        /// </summary>
         public override void TeleopPeriodic()
         {
-
+            SmartDashboard.PutBoolean("Pixy Status", Pixy.IsConnected);
         }
 
-        /// <summary>
-        /// This function is called periodically during test mode
-        /// </summary>
         public override void TestPeriodic()
         {
 
